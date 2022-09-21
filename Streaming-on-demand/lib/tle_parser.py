63175f24a0d253af9a339d26a5be45d0184eb9fa
FranMark <franciscomarquinez@protonmail.com>
2022-09-21 01:58:21 -0300

    Extensión del tle_parser para obtener posición actual del satélite y factor de corrimiento Doppler

--- Streaming-on-demand/lib/tle_parser.py
+++ Streaming-on-demand/lib/tle_parser.py
@@ -1,16 +1,28 @@
-from orbit_predictor.locations import ET_CENTRO_ATOMICO
+from orbit_predictor.locations import ET_CENTRO_ATOMICO, Location
 from orbit_predictor.sources import get_predictor_from_tle_lines
+from orbit_predictor.predictors.accurate import HighAccuracyTLEPredictor
+from datetime import timedelta as td
+
+UTC_OFFSET = -3
+MIN_ELEVATION_ANGLE = 10 #deg
 
 def getNextPass(line1, line2):
     try:
         predictor = get_predictor_from_tle_lines([line1, line2])
-        return predictor.get_next_pass(ET_CENTRO_ATOMICO).aos.strftime("%Y-%m-%d %H:%M:%S")
+        #Está en parámetro when_utc para buscar a partir de otro momento
+        pred = predictor.get_next_pass(ET_CENTRO_ATOMICO, aos_at_dg=MIN_ELEVATION_ANGLE).aos + td(hours=UTC_OFFSET)
+        return pred.strftime("%Y-%m-%d %H:%M:%S")
     except:
         raise ValueError("Invalid TLE")
 
-def getAngles(line1, line2):
+def getCoordinates(line1, line2):
     try:
         predictor = get_predictor_from_tle_lines([line1, line2])
-        return predictor.get_position().elevation_deg, predictor.get_position().azimuth_deg
+        #lat (deg), lon (deg), alt (km)
+        return predictor.get_position().position_llh
     except:
         raise ValueError("Invalid TLE")
+
+def getDopplerFactor(predictor : HighAccuracyTLEPredictor, location : Location):
+    position = predictor.get_position()
+    return location.doppler_factor(position)
