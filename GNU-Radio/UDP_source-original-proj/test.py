388f2405248c0f5a25c00d4bf4d2b136bf74b029
FranMark <franciscomarquinez@protonmail.com>
2022-10-15 06:42:25 -0300

    Pruebas con UDP Source usando la placa con preprocesamiento. Todavía no anda

--- GNU-Radio/UDP_source-original-proj/test.py
+++ GNU-Radio/UDP_source-original-proj/test.py
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: GPL-3.0
 #
 # GNU Radio Python Flow Graph
-# Title: BF for fixed DOA
+# Title: Not titled yet
 # Author: fran
 # GNU Radio version: v3.8.5.0-6-g57bd109d
 
@@ -26,23 +26,21 @@ from gnuradio import qtgui
 from gnuradio.filter import firdes
 import sip
 from gnuradio import blocks
-from gnuradio import digital
 from gnuradio import gr
 import sys
 import signal
 from argparse import ArgumentParser
 from gnuradio.eng_arg import eng_float, intx
 from gnuradio import eng_notation
-from stream_demux import stream_demux_swig
 
 from gnuradio import qtgui
 
-class bf_fixed_doa(gr.top_block, Qt.QWidget):
+class test(gr.top_block, Qt.QWidget):
 
     def __init__(self):
-        gr.top_block.__init__(self, "BF for fixed DOA")
+        gr.top_block.__init__(self, "Not titled yet")
         Qt.QWidget.__init__(self)
-        self.setWindowTitle("BF for fixed DOA")
+        self.setWindowTitle("Not titled yet")
         qtgui.util.check_set_qss()
         try:
             self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
@@ -60,7 +58,7 @@ class bf_fixed_doa(gr.top_block, Qt.QWidget):
         self.top_grid_layout = Qt.QGridLayout()
         self.top_layout.addLayout(self.top_grid_layout)
 
-        self.settings = Qt.QSettings("GNU Radio", "bf_fixed_doa")
+        self.settings = Qt.QSettings("GNU Radio", "test")
 
         try:
             if StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
@@ -78,8 +76,54 @@ class bf_fixed_doa(gr.top_block, Qt.QWidget):
         ##################################################
         # Blocks
         ##################################################
-        self.stream_demux_stream_demux_0 = stream_demux_swig.stream_demux(gr.sizeof_short*1, (44, 32000))
-        self.qtgui_time_sink_x_0 = qtgui.time_sink_c(
+        self.qtgui_time_sink_x_0_0 = qtgui.time_sink_f(
+            1024, #size
+            samp_rate, #samp_rate
+            "", #name
+            1 #number of inputs
+        )
+        self.qtgui_time_sink_x_0_0.set_update_time(0.10)
+        self.qtgui_time_sink_x_0_0.set_y_axis(-1, 1)
+
+        self.qtgui_time_sink_x_0_0.set_y_label('Amplitude', "")
+
+        self.qtgui_time_sink_x_0_0.enable_tags(True)
+        self.qtgui_time_sink_x_0_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, 0, "")
+        self.qtgui_time_sink_x_0_0.enable_autoscale(False)
+        self.qtgui_time_sink_x_0_0.enable_grid(False)
+        self.qtgui_time_sink_x_0_0.enable_axis_labels(True)
+        self.qtgui_time_sink_x_0_0.enable_control_panel(False)
+        self.qtgui_time_sink_x_0_0.enable_stem_plot(False)
+
+
+        labels = ['Signal 1', 'Signal 2', 'Signal 3', 'Signal 4', 'Signal 5',
+            'Signal 6', 'Signal 7', 'Signal 8', 'Signal 9', 'Signal 10']
+        widths = [1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1]
+        colors = ['blue', 'red', 'green', 'black', 'cyan',
+            'magenta', 'yellow', 'dark red', 'dark green', 'dark blue']
+        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
+            1.0, 1.0, 1.0, 1.0, 1.0]
+        styles = [1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1]
+        markers = [-1, -1, -1, -1, -1,
+            -1, -1, -1, -1, -1]
+
+
+        for i in range(1):
+            if len(labels[i]) == 0:
+                self.qtgui_time_sink_x_0_0.set_line_label(i, "Data {0}".format(i))
+            else:
+                self.qtgui_time_sink_x_0_0.set_line_label(i, labels[i])
+            self.qtgui_time_sink_x_0_0.set_line_width(i, widths[i])
+            self.qtgui_time_sink_x_0_0.set_line_color(i, colors[i])
+            self.qtgui_time_sink_x_0_0.set_line_style(i, styles[i])
+            self.qtgui_time_sink_x_0_0.set_line_marker(i, markers[i])
+            self.qtgui_time_sink_x_0_0.set_line_alpha(i, alphas[i])
+
+        self._qtgui_time_sink_x_0_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0_0.pyqwidget(), Qt.QWidget)
+        self.top_layout.addWidget(self._qtgui_time_sink_x_0_0_win)
+        self.qtgui_time_sink_x_0 = qtgui.time_sink_f(
             1024, #size
             samp_rate, #samp_rate
             "", #name
@@ -92,7 +136,7 @@ class bf_fixed_doa(gr.top_block, Qt.QWidget):
 
         self.qtgui_time_sink_x_0.enable_tags(True)
         self.qtgui_time_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, 0, "")
-        self.qtgui_time_sink_x_0.enable_autoscale(True)
+        self.qtgui_time_sink_x_0.enable_autoscale(False)
         self.qtgui_time_sink_x_0.enable_grid(False)
         self.qtgui_time_sink_x_0.enable_axis_labels(True)
         self.qtgui_time_sink_x_0.enable_control_panel(False)
@@ -113,12 +157,9 @@ class bf_fixed_doa(gr.top_block, Qt.QWidget):
             -1, -1, -1, -1, -1]
 
 
-        for i in range(2):
+        for i in range(1):
             if len(labels[i]) == 0:
-                if (i % 2 == 0):
-                    self.qtgui_time_sink_x_0.set_line_label(i, "Re{{Data {0}}}".format(i/2))
-                else:
-                    self.qtgui_time_sink_x_0.set_line_label(i, "Im{{Data {0}}}".format(i/2))
+                self.qtgui_time_sink_x_0.set_line_label(i, "Data {0}".format(i))
             else:
                 self.qtgui_time_sink_x_0.set_line_label(i, labels[i])
             self.qtgui_time_sink_x_0.set_line_width(i, widths[i])
@@ -129,76 +170,34 @@ class bf_fixed_doa(gr.top_block, Qt.QWidget):
 
         self._qtgui_time_sink_x_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0.pyqwidget(), Qt.QWidget)
         self.top_layout.addWidget(self._qtgui_time_sink_x_0_win)
-        self.qtgui_freq_sink_x_0 = qtgui.freq_sink_c(
-            1024, #size
-            firdes.WIN_BLACKMAN_hARRIS, #wintype
-            0, #fc
-            samp_rate, #bw
-            "", #name
-            1
-        )
-        self.qtgui_freq_sink_x_0.set_update_time(0.10)
-        self.qtgui_freq_sink_x_0.set_y_axis(-140, 10)
-        self.qtgui_freq_sink_x_0.set_y_label('Relative Gain', 'dB')
-        self.qtgui_freq_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, 0.0, 0, "")
-        self.qtgui_freq_sink_x_0.enable_autoscale(True)
-        self.qtgui_freq_sink_x_0.enable_grid(False)
-        self.qtgui_freq_sink_x_0.set_fft_average(1.0)
-        self.qtgui_freq_sink_x_0.enable_axis_labels(True)
-        self.qtgui_freq_sink_x_0.enable_control_panel(False)
-
-
-
-        labels = ['', '', '', '', '',
-            '', '', '', '', '']
-        widths = [1, 1, 1, 1, 1,
-            1, 1, 1, 1, 1]
-        colors = ["blue", "red", "green", "black", "cyan",
-            "magenta", "yellow", "dark red", "dark green", "dark blue"]
-        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
-            1.0, 1.0, 1.0, 1.0, 1.0]
-
-        for i in range(1):
-            if len(labels[i]) == 0:
-                self.qtgui_freq_sink_x_0.set_line_label(i, "Data {0}".format(i))
-            else:
-                self.qtgui_freq_sink_x_0.set_line_label(i, labels[i])
-            self.qtgui_freq_sink_x_0.set_line_width(i, widths[i])
-            self.qtgui_freq_sink_x_0.set_line_color(i, colors[i])
-            self.qtgui_freq_sink_x_0.set_line_alpha(i, alphas[i])
-
-        self._qtgui_freq_sink_x_0_win = sip.wrapinstance(self.qtgui_freq_sink_x_0.pyqwidget(), Qt.QWidget)
-        self.top_layout.addWidget(self._qtgui_freq_sink_x_0_win)
-        self.digital_crc32_async_bb_0 = digital.crc32_async_bb(True)
-        self.blocks_vector_to_streams_0 = blocks.vector_to_streams(gr.sizeof_gr_complex*1, 16)
-        self.blocks_vector_sink_x_0 = blocks.vector_sink_s(1, 1024)
-        self.blocks_udp_source_0 = blocks.udp_source(gr.sizeof_short*1, '192.168.0.21', 12345, 32044, True)
-        self.blocks_stream_to_vector_0 = blocks.stream_to_vector(gr.sizeof_gr_complex*1, 16)
-        self.blocks_null_sink_0_3 = blocks.null_sink(gr.sizeof_gr_complex*1)
-        self.blocks_null_sink_0_2_0 = blocks.null_sink(gr.sizeof_gr_complex*1)
-        self.blocks_null_sink_0_2 = blocks.null_sink(gr.sizeof_gr_complex*1)
-        self.blocks_null_sink_0_1_1 = blocks.null_sink(gr.sizeof_gr_complex*1)
-        self.blocks_null_sink_0_1_0 = blocks.null_sink(gr.sizeof_gr_complex*1)
-        self.blocks_null_sink_0_1 = blocks.null_sink(gr.sizeof_gr_complex*1)
-        self.blocks_null_sink_0_0_2 = blocks.null_sink(gr.sizeof_gr_complex*1)
-        self.blocks_null_sink_0_0_1_0_0 = blocks.null_sink(gr.sizeof_gr_complex*1)
-        self.blocks_null_sink_0_0_1_0 = blocks.null_sink(gr.sizeof_gr_complex*1)
-        self.blocks_null_sink_0_0_1 = blocks.null_sink(gr.sizeof_gr_complex*1)
-        self.blocks_null_sink_0_0_0_1 = blocks.null_sink(gr.sizeof_gr_complex*1)
-        self.blocks_null_sink_0_0_0_0 = blocks.null_sink(gr.sizeof_gr_complex*1)
-        self.blocks_null_sink_0_0_0 = blocks.null_sink(gr.sizeof_gr_complex*1)
-        self.blocks_null_sink_0_0 = blocks.null_sink(gr.sizeof_gr_complex*1)
-        self.blocks_null_sink_0 = blocks.null_sink(gr.sizeof_gr_complex*1)
-        self.blocks_message_debug_0 = blocks.message_debug()
-        self.blocks_interleaved_short_to_complex_0 = blocks.interleaved_short_to_complex(False, False)
+        self.blocks_vector_to_streams_0 = blocks.vector_to_streams(gr.sizeof_float*1, 16)
+        self.blocks_udp_source_0_0_0 = blocks.udp_source(gr.sizeof_int*1, '192.168.0.21', 12345, 1472, True)
+        self.blocks_stream_to_vector_0 = blocks.stream_to_vector(gr.sizeof_float*1, 16)
+        self.blocks_null_sink_0_3 = blocks.null_sink(gr.sizeof_float*1)
+        self.blocks_null_sink_0_2_0 = blocks.null_sink(gr.sizeof_float*1)
+        self.blocks_null_sink_0_2 = blocks.null_sink(gr.sizeof_float*1)
+        self.blocks_null_sink_0_1_1 = blocks.null_sink(gr.sizeof_float*1)
+        self.blocks_null_sink_0_1_0 = blocks.null_sink(gr.sizeof_float*1)
+        self.blocks_null_sink_0_1 = blocks.null_sink(gr.sizeof_float*1)
+        self.blocks_null_sink_0_0_2 = blocks.null_sink(gr.sizeof_float*1)
+        self.blocks_null_sink_0_0_1_0_0 = blocks.null_sink(gr.sizeof_float*1)
+        self.blocks_null_sink_0_0_1_0 = blocks.null_sink(gr.sizeof_float*1)
+        self.blocks_null_sink_0_0_1 = blocks.null_sink(gr.sizeof_float*1)
+        self.blocks_null_sink_0_0_0_1 = blocks.null_sink(gr.sizeof_float*1)
+        self.blocks_null_sink_0_0_0_0 = blocks.null_sink(gr.sizeof_float*1)
+        self.blocks_null_sink_0_0_0 = blocks.null_sink(gr.sizeof_float*1)
+        self.blocks_null_sink_0_0 = blocks.null_sink(gr.sizeof_float*1)
+        self.blocks_null_sink_0 = blocks.null_sink(gr.sizeof_float*1)
+        self.blocks_int_to_float_0 = blocks.int_to_float(1, 1)
 
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.blocks_interleaved_short_to_complex_0, 0), (self.blocks_stream_to_vector_0, 0))
+        self.connect((self.blocks_int_to_float_0, 0), (self.blocks_stream_to_vector_0, 0))
+        self.connect((self.blocks_int_to_float_0, 0), (self.qtgui_time_sink_x_0_0, 0))
         self.connect((self.blocks_stream_to_vector_0, 0), (self.blocks_vector_to_streams_0, 0))
-        self.connect((self.blocks_udp_source_0, 0), (self.stream_demux_stream_demux_0, 0))
+        self.connect((self.blocks_udp_source_0_0_0, 0), (self.blocks_int_to_float_0, 0))
         self.connect((self.blocks_vector_to_streams_0, 3), (self.blocks_null_sink_0, 0))
         self.connect((self.blocks_vector_to_streams_0, 14), (self.blocks_null_sink_0_0, 0))
         self.connect((self.blocks_vector_to_streams_0, 12), (self.blocks_null_sink_0_0_0, 0))
@@ -214,14 +213,11 @@ class bf_fixed_doa(gr.top_block, Qt.QWidget):
         self.connect((self.blocks_vector_to_streams_0, 11), (self.blocks_null_sink_0_2, 0))
         self.connect((self.blocks_vector_to_streams_0, 0), (self.blocks_null_sink_0_2_0, 0))
         self.connect((self.blocks_vector_to_streams_0, 7), (self.blocks_null_sink_0_3, 0))
-        self.connect((self.blocks_vector_to_streams_0, 15), (self.qtgui_freq_sink_x_0, 0))
         self.connect((self.blocks_vector_to_streams_0, 15), (self.qtgui_time_sink_x_0, 0))
-        self.connect((self.stream_demux_stream_demux_0, 1), (self.blocks_interleaved_short_to_complex_0, 0))
-        self.connect((self.stream_demux_stream_demux_0, 0), (self.blocks_vector_sink_x_0, 0))
 
 
     def closeEvent(self, event):
-        self.settings = Qt.QSettings("GNU Radio", "bf_fixed_doa")
+        self.settings = Qt.QSettings("GNU Radio", "test")
         self.settings.setValue("geometry", self.saveGeometry())
         event.accept()
 
@@ -230,14 +226,14 @@ class bf_fixed_doa(gr.top_block, Qt.QWidget):
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
-        self.qtgui_freq_sink_x_0.set_frequency_range(0, self.samp_rate)
         self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate)
+        self.qtgui_time_sink_x_0_0.set_samp_rate(self.samp_rate)
 
 
 
 
 
-def main(top_block_cls=bf_fixed_doa, options=None):
+def main(top_block_cls=test, options=None):
 
     if StrictVersion("4.5.0") <= StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
         style = gr.prefs().get_string('qtgui', 'style', 'raster')
