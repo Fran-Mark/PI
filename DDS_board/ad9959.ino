3b6b676413f90761b0d800ae4810926b7e602a16
José Quinteros del Castillo <jquinteros@ib.edu.ar>
2022-11-08 18:20:33 -0300

    Add source files for DDS board

---
+++ DDS_board/ad9959.ino
@@ -0,0 +1,244 @@
+/*********************************************************************************/
+/***************** SETUP AND COMMUNICATION WITH DDS ANALOG AD9959 ****************/ 
+/*********************************************************************************/
+
+/**
+    Using the first SPI port (SPI_1)
+    SS    <-->  PA4 <-->  BOARD_SPI1_NSS_PIN
+    SCK   <-->  PA5 <-->  BOARD_SPI1_SCK_PIN
+    MISO  <-->  PA6 <-->  BOARD_SPI1_MISO_PIN
+    MOSI  <-->  PA7 <-->  BOARD_SPI1_MOSI_PIN
+**/
+
+#include <SPI.h>
+
+#define DDS_CS  PA4    //SPI_1 Chip Select pin is PA4
+#define DDS_P0  PB12
+#define DDS_SD1 PA0
+#define DDS_SD3 PA1
+#define DDS_RST PA2
+#define DDS_UP  PA3
+#define DDS_PDC PB14   // Power-Down Control
+
+#define READ_DDS    0x80
+#define WRITE_DDS   0x00
+
+#define F_S         25000000  // System clock fs = 25MHz
+#define PLL         20
+
+uint32_t freq_test = 0;
+byte data;
+
+/********************************* TEST FUNCTIONS ********************************/ 
+
+void testSPI()
+{
+  digitalWrite(DDS_CS, LOW); // manually take CSN low for SPI_1 transmission
+
+  SPI.transfer(READ_DDS | 0x00);
+  data = SPI.transfer(0x00);
+
+  digitalWrite(DDS_CS, HIGH); // manually take CSN high between spi transmissions
+
+  Serial.print(data);
+  Serial.print("\r");
+}
+
+void testDDS()    // Test all the parameters (frequency, phase, amplitude) of DDS
+{
+  uint32_t freq;
+  float phase, amplitude;
+
+//  channelSel(0);
+//  freq = 100000000;
+//  writeFreq(freq);
+//  amplitude = 1.0;
+//  writeAmplitude(amplitude);
+//  
+//  channelSel(1);
+//  freq = 100000000;
+//  writeFreq(freq);
+//  amplitude = 1.0;
+//  writeAmplitude(amplitude);
+//
+//  channelSel(2);
+//  freq = 100000000;
+//  writeFreq(freq);
+//  amplitude = 1.0;
+//  writeAmplitude(amplitude);
+//  phase = 120.0;
+//  writePhase(phase);
+//
+//  channelSel(3);
+//  freq = 100000000;
+//  writeFreq(freq);
+//  amplitude = 1.0;
+//  writeAmplitude(amplitude);
+//  phase = 240.0;
+//  writePhase(phase);
+
+  channelSel(4);    // all channels
+  freq = 150000000;
+  writeFreq(freq);
+//  amplitude = 1.0;
+//  writeAmplitude(amplitude);
+//  phase = 240.0;
+//  writePhase(phase);
+
+  pulseUpdate();        //update output
+
+}
+
+void testFreq()     // Increase frequency with steps of 10MHz to analize filter characteristics of DDS
+{
+  channelSel(4);    // all channels
+  freq_test = freq_test + 10000000;
+  if ( freq_test > 250000000 ) freq_test = 0;
+  writeFreq(freq_test);
+
+  pulseUpdate();    //update output to the new frequency
+}
+
+/********************************* ACTUAL FUNCTIONS ********************************/ 
+
+void setupDDS()
+{
+  /* SETUP DIO PINS */
+  pinMode(DDS_P0, OUTPUT);
+  digitalWrite(DDS_P0, LOW); // Channel Frequency Tuning Word 0 (Register 0x04) is chosen
+
+  pinMode(DDS_SD1, OUTPUT);
+  digitalWrite(DDS_SD1, LOW); // If SD1 not used, set to 0
+
+  pinMode(DDS_SD3, OUTPUT);
+  digitalWrite(DDS_SD3, LOW); // If SD3 not used, must be set to 0
+
+  pinMode(DDS_RST, OUTPUT);
+  digitalWrite(DDS_RST, LOW); // RESET
+
+  pinMode(DDS_UP, OUTPUT);
+  digitalWrite(DDS_UP, LOW); // IO_UPDATE
+
+  pinMode(DDS_PDC, OUTPUT);
+  digitalWrite(DDS_PDC, LOW); // Power-Down Control
+
+  pinMode(DDS_CS, OUTPUT);
+  digitalWrite(DDS_CS, HIGH); // CHIP_SELECT
+
+  /* SETUP SPI COMMUNICATION */
+  SPI.begin(); //Initialize the SPI_1 port.
+  SPI.setBitOrder(MSBFIRST); // Set the SPI_1 bit order
+  SPI.setDataMode(SPI_MODE0); //Set the  SPI_1 data mode 0
+  SPI.setClockDivider(SPI_CLOCK_DIV16);      // 72/2 = 36 // Slow speed (72 / 16 = 4.5 MHz SPI_1 speed)
+
+  /* SETUP DDS */
+  resetDDS();       // Forces internal registers to default state
+  initializeDDS();  // Initialize register CSR, FR1, FR2 and CFR
+  pulseUpdate();    // IO_UPDATE
+}
+
+void resetDDS() //master resets the DDS
+{
+  digitalWrite(DDS_RST, HIGH);
+  digitalWrite(DDS_RST, LOW);
+}
+
+void resetCommunication() //resets DDS internal communication (using before new command adds protection against previous bad writes)
+{
+  digitalWrite(DDS_CS, HIGH);
+  digitalWrite(DDS_SD3, HIGH);
+  digitalWrite(DDS_SD3, LOW);
+  digitalWrite(DDS_CS, LOW);
+}
+
+void spiWrite(byte data[], int numBytes)  //writes an array of bytes to the DDS board. Takes an array of bytes to write, the length of the array, and SPI settings.
+{
+  resetCommunication();
+  for (int i = 0; i < numBytes; i++) //for all bytes in byte array
+  {
+    SPI.transfer(data[i]);//write byte
+  }
+}
+
+void initializeDDS() //writes all of the static settings to the DDS
+{
+  resetDDS();
+
+  digitalWrite(DDS_CS, LOW);
+  delay(1);
+
+  //CSR Channel Select Register
+  {
+    byte valsToWrite[] = {B00000000, B11110010}; // 4 channels activated, single bit - 3 wires com, MSB
+    spiWrite(valsToWrite, 2);
+  }
+
+  //FR1 Function Register 1
+  {
+    byte valsToWrite[] = {
+      B00000001,  // Address 0x01
+      1 << 7 | PLL << 2,   // VCO gain [23], PLL divider [22:18]
+      B00000000,
+      B00000000
+    };
+    spiWrite(valsToWrite, 4);
+  }
+
+  digitalWrite(DDS_CS, HIGH);
+}
+
+void pulseUpdate() //pulses an update to renew the frequency
+{
+  digitalWrite(DDS_UP, HIGH);
+  digitalWrite(DDS_UP, LOW);
+}
+
+void channelSel(int chNO) //select a channel (0,1,2,3) to write to.  4 selects all channels
+{
+  byte chans[] = {B00010000, B00100000, B01000000, B10000000, B11110000};
+  {
+    byte valsToWrite[] = {B00000000, chans[chNO] | B00000010};
+    spiWrite(valsToWrite, 2);
+  }
+}
+
+void writeFreq(double frequency) //writes a new frequency to the selected channel
+{
+  uint32_t ftw = (uint32_t)(frequency / ( F_S * PLL ) * 4294967296 );
+  if ( ftw > 4294967295 ) ftw = 4294967295;   // maximum value 2^32 - 1 = 4294967295
+
+  //CFTW0 Channel Frequency Tuning Word 0
+  byte writeCFTW0_1 = ( byte )( ( ftw >> 24 ) & 0xff );
+  byte writeCFTW0_2 = ( byte )( ( ftw >> 16 ) & 0xff );
+  byte writeCFTW0_3 = ( byte )( ( ftw >> 8 ) & 0xff );
+  byte writeCFTW0_4 = ( byte )( ftw & 0xff );
+  byte valsToWrite[] = {B00000100, writeCFTW0_1, writeCFTW0_2, writeCFTW0_3, writeCFTW0_4};
+  spiWrite(valsToWrite, 5);
+}
+
+void writePhase(float phase) //writes a new phase offset to the selected channel
+{
+  while ( phase >= 360.0 ) phase -= 360.0;
+  
+  uint16_t phow = ( uint16_t )( phase * 16384 / 360 );
+  if ( phow > 16383 ) phow = 16383;   // maximum value 2^14 - 1 = 16383
+
+  //CPOW0 Channel Phase Offset Word 0
+  byte writeCPOW0_1 = ( byte )( ( phow >> 8 ) & 0xff );
+  byte writeCPOW0_2 = ( byte )( phow & 0xff );
+  byte valsToWrite[] = {B00000101, writeCPOW0_1, writeCPOW0_2};
+  spiWrite(valsToWrite, 3);
+}
+
+void writeAmplitude(float amplitude) //writes a new amplitude scale factor to the selected channel
+{
+  uint16_t asf = ( uint16_t )( amplitude * 1024 );
+  if ( asf > 1023 ) asf = 1023;   // maximum value 2^10 - 1 = 1023
+
+  //ACR Amplitude Control Register
+  byte writeACR_1 = B00000000;
+  byte writeACR_2 = ( B00010000 ) | ( byte )( ( asf >> 8 ) & 0xff ); // 1 << 3 Amplitude multiplier enable [12]
+  byte writeACR_3 = ( byte )( asf & 0xff );
+  byte valsToWrite[] = {B00000110, writeACR_1, writeACR_2, writeACR_3};
+  spiWrite(valsToWrite, 4);
+}
---
+++ DDS_board/array_antenna_emulator.ino
@@ -0,0 +1,123 @@
+/*********************************************************************************/
+/*********************** EMULATOR OF THE SIGNALS RECEIVED ************************/
+/********************** BY THE ELEMENTS OF AN ARRAY ANTENNA **********************/
+/************************ IN LEO SATELLITES COMMUNICATION ************************/ 
+/*********************************************************************************/
+
+//#define TIME_SCANNING       // define to scan over time, not define to scan over theta
+
+#ifdef TIME_SCANNING        // if time scanning, declare t as global variable
+double t = 0;               // Time [s]
+#else                       // if theta scanning, declare theta as global variable
+double theta = -1.5708;     // Angle relative to broadside [rad], starting angle = -90deg
+#endif
+
+#define INTERRUPT_PERIOD    100000   // in microseconds, 1000 gives 1 kHz
+#define TIME_ACCEL  50      // define TIME_ACCEL 1 to have time of simulation correlated to real-life time         
+
+HardwareTimer timer(2);     // Timer 2 for interruption
+
+void setup() {
+  /* SETUP UART */
+  Serial.begin(115200, SERIAL_8N1);
+
+  /* SETUP DDS */
+  setupDDS();
+  
+  /* SETUP SIGNALS MODEL */
+  delay(100);  // so the terminal catches the start-up messages
+  derive_variables();
+
+  /* SETUP TIMER INTERRUPT */
+  timer.pause();  // Pause the timer while we're configuring it
+  timer.setPeriod(INTERRUPT_PERIOD); // Set up period in microseconds
+
+  // Set up an interrupt on channel 1
+  timer.setChannel1Mode(TIMER_OUTPUT_COMPARE);
+  timer.setCompare(TIMER_CH1, 1);               // Interrupt 1 count after each update
+  timer.attachCompare1Interrupt(handler_int
+  );       // Interruption handler function: handler_int (change to testDDS or testFreq, at signals_model.ino, for testing the DDS)
+
+  timer.refresh();  // Refresh the timer's count, prescale, and overflow
+  timer.resume();   // Start the timer counting
+}
+
+
+void loop( )
+{
+  //
+}
+
+
+void handler_int( )
+{
+  double d;
+  double f;
+  double phi_0, phi_1, phi_2, phi_3, ampl; 
+  
+  #ifdef TIME_SCANNING
+  double theta;
+  extern double t_end;
+  if( t > t_end ) t = 0.0;
+
+  d = distance_satellite_antenna_t( t );
+  theta = angle_relative_to_broadside ( t, d );
+
+  #else
+  if( theta > 1.5708 ) theta = -1.5708;
+  d = distance_satellite_antenna_theta ( theta );
+  
+  #endif
+  
+  f = frequency( theta );
+  phi_0 = phase( 1, theta );
+  phi_1 = phase( 2, theta );
+  phi_2 = phase( 3, theta );
+  phi_3 = phase( 4, theta );
+  ampl = amplitude( d );
+  
+  /* PRINT VARIABLES (CAN'T DO IT IF TIME_SCANNING TOO LOW */
+//  Serial.print( String( (String)"t = " + t + "\r" ) );
+//  Serial.print( String( (String)"d = " + d + "\r" ) );
+  Serial.print( String( (String)"theta = " + theta * 57.296 + "\r" ) );
+//  Serial.print( String( (String)"f = " + f + "\r" ) );
+//  Serial.print( String( (String)"phi_0 = " + phi_0 + "\r" ) );
+//  Serial.print( String( (String)"phi_1 = " + phi_1 + "\r" ) );
+//  Serial.print( String( (String)"phi_2 = " + phi_2 + "\r" ) );
+//  Serial.print( String( (String)"phi_3 = " + phi_3 + "\r" ) );
+//  Serial.print( String( (String)"ampl = " + ampl + "\r\r" ) );
+//  Serial.print( String( (String)"[ " + t + "    " + d + "    " + theta + "    " + f + "    " + phi_0 + "    " + phi_1 + "    " + phi_2 + "    " + phi_3 + "    " + ampl + "   ]\r\r" ) );
+
+  /* MODIFY ALL CHANNEL FREQUENCY AND AMPLITUDE */
+  channelSel(4);             //select a channel (0,1,2,3) to write to.  4 selects all channels
+  writeFreq(f);
+  writeAmplitude(ampl);
+
+  /* MODIFY CHANNEL 2 SIGNAL */
+  channelSel(0);
+  writePhase(phi_0);
+
+  channelSel(1);
+  writePhase(phi_1);
+
+  channelSel(2);
+  writePhase(phi_2);
+
+  channelSel(3);
+  writePhase(phi_3);
+
+  /* UPDATE CHANGES */
+  pulseUpdate();        //update output to the new frequency
+
+  /* UPDATE TIME/THETA COUNTER */
+  #ifdef TIME_SCANNING
+  t = t + (double)INTERRUPT_PERIOD/1000000.0 * TIME_ACCEL;   // t will correspond to real-life time (accelerated by TIME_ACCEL)
+//  t = t + t_end/16.0;                                      // will scan the pass over 16 discreet points
+
+  #else
+  theta = theta + 0.00872665;   //steps of 0.5deg, for 1.0deg, use 0.0174533;
+  
+  #endif
+  
+
+}
